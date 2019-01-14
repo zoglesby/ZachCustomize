3a792c59dcfb88f08d84caf7df5c9576ee2ecf19
Jonathan LaCour <jonathan@cleverdevil.org>
2019-01-14 23:58:20 +0000

    Create Listen entries at the appropriate date/time.

--- Pages/ListenEndpoint.php
+++ Pages/ListenEndpoint.php
@@ -22,12 +22,14 @@
                 $this->setInput('listenType', $json->type);
                 $this->setInput('listenAuthor', $json->author);
                 $this->setInput('mediaURL', $json->link);
+                $this->setInput('created', strtotime($json->listenDateTime));
                 
                 $contentType = new \IdnoPlugins\Listen\ContentType();
                 
                 $entity = $contentType->createEntity();
                 $entity->setOwner('http://cleverdevil.io/profile/cleverdevil');
-                
+                $entity->created = strtotime($json->listenDateTime); 
+
                 $result = $entity->saveDataFromInput();
                 if ($result) {
                     \Idno\Core\Idno::site()->triggerEvent('listen/post/success', ['page' => $this, 'object' => $entity]);
